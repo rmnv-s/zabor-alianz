ac496776d0e01d9c2d740cc1a3fc16ba01082271
Stas Remnev <remnev.dev@gmail.com>
2024-02-07 17:49:16 +0300

    style: страница для 1024

--- src/js/script.js
+++ src/js/script.js
@@ -1,66 +1,50 @@
 const swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
   direction: "horizontal",
   loop: true,
-
-  // If we need pagination
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
   },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  // And if we need scrollbar
   scrollbar: {
     el: ".swiper-scrollbar",
   },
 });
 
-const swiperComments = new Swiper(".swiper-comment", {
-  direction: "horizontal",
-  slidesPerView: 3,
-  spaceBetween: 60,
-  loop: true,
+// const swiperComment = new Swiper(".swiper-comment", {
+//   slidesPerView: 3,
+//   spaceBetween: 30,
+//   loop: true,
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   pagination: {
+//     el: ".swiper-pagination-comment",
+//     clickable: true,
+//   },
+// });
 
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
-});
-
+// console.log(swiperComments);
 // INPUT Длинна забора
-const formCalculate = document.querySelector(".calculate__form");
-const inputNumberLengthFence = formCalculate.querySelector(
-  ".input-length__fence"
-);
-// const formInputError = document.querySelector(".form-input__error");
-
-inputNumberLengthFence.addEventListener("keydown", function (evt) {
-  // Проверяем, была ли введена цифра
-  if (Number.isNaN(Number(evt.key))) {
-    // Если пользователь ввёл не цифру, показываем блок с ошибкой
-    // formInputError.style.display = "block";
-    // evt.preventDefault();
-    console.log("нужны цифры");
-  } else if (
-    inputNumberLengthFence.value.length >= 4 &&
-    evt.key !== "Backspace"
-  ) {
-    evt.preventDefault();
-    console.log("притормози");
-  }
-});
+// const formCalculate = document.querySelector(".calculate__form");
+// const inputNumberLengthFence = formCalculate.querySelector(
+//   ".input-length__fence"
+// );
+// // const formInputError = document.querySelector(".form-input__error");
+//
+// inputNumberLengthFence.addEventListener("keydown", function (evt) {
+//   // Проверяем, была ли введена цифра
+//   if (Number.isNaN(Number(evt.key))) {
+//     // Если пользователь ввёл не цифру, показываем блок с ошибкой
+//     // formInputError.style.display = "block";
+//     // evt.preventDefault();
+//     console.log("нужны цифры");
+//   } else if (
+//     inputNumberLengthFence.value.length >= 4 &&
+//     evt.key !== "Backspace"
+//   ) {
+//     evt.preventDefault();
+//     console.log("притормози");
+//   }
+// });
