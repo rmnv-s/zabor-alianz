d6db397d13b592990e868f36d1f4b1a86e6bdd86
Stas Remnev <remnev.dev@gmail.com>
2024-02-13 13:31:13 +0300

    fix: бургер меню ссылки

--- src/js/script.js
+++ src/js/script.js
@@ -27,44 +27,6 @@ window.addEventListener("scroll", function () {
   }
 });
 
-// const swiperComment = new Swiper(".swiper-comment", {
-//   slidesPerView: 3,
-//   spaceBetween: 30,
-//   loop: true,
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-//   pagination: {
-//     el: ".swiper-pagination-comment",
-//     clickable: true,
-//   },
-// });
-
-// console.log(swiperComments);
-// INPUT Длинна забора
-// const formCalculate = document.querySelector(".calculate__form");
-// const inputNumberLengthFence = formCalculate.querySelector(
-//   ".input-length__fence"
-// );
-// // const formInputError = document.querySelector(".form-input__error");
-//
-// inputNumberLengthFence.addEventListener("keydown", function (evt) {
-//   // Проверяем, была ли введена цифра
-//   if (Number.isNaN(Number(evt.key))) {
-//     // Если пользователь ввёл не цифру, показываем блок с ошибкой
-//     // formInputError.style.display = "block";
-//     // evt.preventDefault();
-//     console.log("нужны цифры");
-//   } else if (
-//     inputNumberLengthFence.value.length >= 4 &&
-//     evt.key !== "Backspace"
-//   ) {
-//     evt.preventDefault();
-//     console.log("притормози");
-//   }
-// });
-
 // БУРГЕР МЕНЮ
 let menuBtn = document.querySelector(".burger-menu");
 const body = document.querySelector("body");
@@ -94,4 +56,29 @@ menuBtn.addEventListener("click", () => {
     calling.classList.remove("open");
     headerPhone.classList.remove("header__phone-open-burger");
   }
+});
+
+const callBtn = document.querySelector(".calling__btn-online");
+const menuLinks = document.querySelectorAll(".main-nav a");
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    menuBtn.classList.toggle("active");
+    header.classList.remove("menu-open");
+    body.classList.remove("no-scroll");
+
+    mainNav.classList.remove("open");
+    calling.classList.remove("open");
+    headerPhone.classList.remove("header__phone-open-burger");
+  });
+});
+
+callBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("active");
+  header.classList.remove("menu-open");
+  body.classList.remove("no-scroll");
+
+  mainNav.classList.remove("open");
+  calling.classList.remove("open");
+  headerPhone.classList.remove("header__phone-open-burger");
 });
